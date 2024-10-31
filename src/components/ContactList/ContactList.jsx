5165b939d6c72ba82d87414949deb05485a86fd2
Igor Tymchuk <167434833+Igor-Tymchuk@users.noreply.github.com>
2024-10-31 13:51:59 +0200

    add worked contactList

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,10 @@
+import Contact from "../Contact/Contact";
+
+const ContactList = ({ contactList }) => {
+    return <ul>
+        {contactList.map(contact => <Contact key={contact.id} contact={contact} />)}
+    </ul>
+};
+
+
+export default ContactList;
